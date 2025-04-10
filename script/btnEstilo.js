0b73cf9c14f0b7f6e01b78cd51a960cb832c11fe
John Ávylan <avylanjohn@gmail.com>
2025-04-10 14:01:24 -0300

    implementando mobile firts

--- script/btnEstilo.js
+++ script/btnEstilo.js
@@ -1,8 +1,28 @@
-const menuEstilo = document.getElementById('menu-estilo');
+const botaoEstilo = document.querySelector(".btn-estilo");
+const menuEstilo = document.getElementById("menu-estilo");
+let menuAberto = false;
+
+botaoEstilo.addEventListener("click", mostrarMenuEstilo);
 
 function mostrarMenuEstilo() {
-    if (menuEstilo.style.display === 'block'){
-        menuEstilo.style.display = 'none';
+    if (!menuAberto) {
+        menuEstilo.classList.remove("fechar");
+        menuEstilo.classList.add("ativo");
+        menuAberto = true;
+    } else {
+        menuEstilo.classList.remove("ativo");
+        menuEstilo.classList.add("fechar");
+        menuAberto = false;
     }
-    else{menuEstilo.style.display = 'block';}
 }
+
+document.addEventListener("click", function (event) {
+    const dentroDoMenu = menuEstilo.contains(event.target);
+    const clicouNoBotao = botaoEstilo.contains(event.target);
+
+    if (!dentroDoMenu && !clicouNoBotao && menuAberto) {
+        menuEstilo.classList.remove("ativo");
+        menuEstilo.classList.add("fechar");
+        menuAberto = false;
+    }
+});
