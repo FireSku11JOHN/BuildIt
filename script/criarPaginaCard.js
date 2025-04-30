370a3e5920ad41d0dfad36fce7d5dbaa6e53ed3d
John Ávylan <avylanjohn@gmail.com>
2025-04-30 08:28:34 -0300

    Renomenado "pagina_card.html" para "card.html"

--- script/criarPaginaCard.js
+++ script/criarPaginaCard.js
@@ -2,6 +2,6 @@
 
 export function criarPaginaCard(cardDados){
     localStorage.setItem('cardSelecionado', JSON.stringify(cardDados));
-    window.location.href = "./pages/pagina_card.html"; 
+    window.location.href = "./pages/card.html"; 
 }
     
