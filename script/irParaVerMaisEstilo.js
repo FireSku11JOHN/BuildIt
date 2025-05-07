a1a96784826d0d9d921fcd582b096b1143059c0b
John Ávylan <avylanjohn@gmail.com>
2025-05-07 13:32:28 -0300

    Add ação dos botões de estilos

---
+++ script/irParaVerMaisEstilo.js
@@ -0,0 +1,24 @@
+import { dadosEstilos } from "./dadosEstilos.js";
+
+function normalizarTag(str) {
+    return str
+      .normalize("NFD")                         // Remove acentos
+      .replace(/[\u0300-\u036f]/g, "")         // Remove marcas de acento
+      .toLowerCase()                           // Converte para minúsculo
+      .replace(/\s+/g, '-');                   // Substitui espaços por traços
+  }
+  
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('menu-estilo').addEventListener('click', (e) => {
+        const bnt = e.target.closest('.btnEstilo');
+        if (bnt) {
+            console.log('Botão clicado:',bnt.textContent);
+            const tagEstilo = dadosEstilos.find(id => id.tag === normalizarTag(bnt.textContent));
+            if (tagEstilo) {
+                console.log('Tag do estilo:', tagEstilo.tag);
+                window.location.href = `/pages/todosCardsEstilos.html?estilo=${encodeURIComponent(tagEstilo.tag)}`;               
+            }
+        }
+    })  
+});
