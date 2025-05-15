da7c4f012a299046632b43d598db4a65dd8b38c8
John Ávylan <avylanjohn@gmail.com>
2025-05-15 13:30:58 -0300

    up campo de busca

--- script/bntBuscar.js
+++ script/bntBuscar.js
@@ -5,27 +5,36 @@ const inputBuscar = document.querySelector("#input-buscar");
 
 function buscarTutorial() {
     const inputValue = inputBuscar.value.trim().toLowerCase();
+    const resEscritoInput = document.getElementById("res-escrito")
+
+     // Atualiza a li id= res-ecrito com o que foi digitado
+    resEscritoInput.innerText = inputValue ? `${inputValue}` : "";
+
+    // Remove apenas os cards anteriores
+    const cardsAnteriores = campoResultado.querySelectorAll(".li-card-busca");
+    cardsAnteriores.forEach(card => card.remove());
+
     const resultados = dadosCardSection.filter((item) => {
         return (
-            item.titulo.toLowerCase().includes(inputValue) ||
-            item.descricao.toLowerCase().includes(inputValue)
+            item.titulo.toLowerCase().includes(inputValue)                                                           
+            // item.descricao.toLowerCase().includes(inputValue)
         );
     });
 
-    // Limpa os resultados anteriores
-    campoResultado.innerHTML = "";
-
-    if (resultados.length === 0) {
-        campoResultado.innerHTML = "<p>Nenhum resultado encontrado.</p>";
+    if (resultados.length === 0 && inputValue !== "") {
+        const li = document.createElement("li");
+        li.textContent = "Nenhum resultado encontrado.";
+        li.classList.add("li-card-busca");
+        campoResultado.appendChild(li);
         return;
     }
+
 
     // Cria e exibe os cards
     resultados.forEach((item) => {
         const card = document.createElement("li");
         card.classList.add("li-card-busca");
         card.innerHTML = `
-            <img class="img-card-busca" src="${item.imagem}" alt="${item.titulo}" />
             <div class="info">
                 <h3>${item.titulo}</h3>
             </div>
@@ -35,7 +44,6 @@ function buscarTutorial() {
 }
 
 bntBuscar.addEventListener("click",buscarTutorial);
-
 
 inputBuscar.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
@@ -49,15 +57,18 @@ const campoBusca = document.querySelector(".campo-busca");
 const campoResultado = document.querySelector("#campo-resultado");
 
 // Abrir campo de reultados
-inputBuscar.addEventListener("focus", function () {
-    campoResultado.style.display = "flex";
-    campoBusca.style.borderRadius = "20px 20px 0px 0px";
-});
+// inputBuscar.addEventListener("focus", function () {
+//     campoResultado.style.display = "flex";
+//     campoBusca.style.borderRadius = "20px 20px 0px 0px";
+// });
 
 // Detectar digitação (pega corretamente o valor atualizado)
 inputBuscar.addEventListener("input", function () {
     const inputValue = inputBuscar.value;
     console.log(inputValue);
+    campoResultado.style.display = "flex";
+    campoBusca.style.borderRadius = "20px 20px 0px 0px";
+    buscarTutorial();
 });
 
 // Detectar clique fora para fechar o resultado
