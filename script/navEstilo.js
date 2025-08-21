4e4529773e1931d9eb3371d20fe594b777ab6767
John Ávylan <avylanjohn@gmail.com>
2025-08-21 10:24:49 -0300

    Melhorada a função de exibir o menu de estilos a partir do header

--- script/navEstilo.js
+++ script/navEstilo.js
@@ -1,28 +1,54 @@
+import { dadosEstilos } from "../dados/dadosEstilos.js";
+
 const botaoEstilo = document.querySelector(".btn-estilo");
 const menuEstilo = document.getElementById("menu-estilo");
+
 let menuAberto = false;
 
-botaoEstilo.addEventListener("click", mostrarMenuEstilo);
+function criarMenuEstilos() {
+    const ul = document.createElement("ul");
+    ul.classList.add("menu-estilo-lista");
 
-function mostrarMenuEstilo() {
-    if (!menuAberto) {
-        menuEstilo.classList.remove("fechar");
-        menuEstilo.classList.add("ativo");
-        menuAberto = true;
+    dadosEstilos.forEach(estilo => {
+        const li = document.createElement("li");
+        li.classList.add("menu-estilo_item");
+        li.classList.add("btnEstilo");
+        li.textContent = estilo.nome;
+        ul.appendChild(li);
+    });
+
+    menuEstilo.appendChild(ul);
+}
+
+function abrirMenu() {
+    menuEstilo.classList.remove("fechar");
+    menuEstilo.classList.add("ativo");
+    menuAberto = true;
+}
+
+function fecharMenu() {
+    menuEstilo.classList.remove("ativo");
+    menuEstilo.classList.add("fechar");
+    menuAberto = false;
+}
+
+function toggleMenu() {
+    if (menuAberto) {
+        fecharMenu();
     } else {
-        menuEstilo.classList.remove("ativo");
-        menuEstilo.classList.add("fechar");
-        menuAberto = false;
+        abrirMenu();
     }
 }
+
+criarMenuEstilos();
+
+botaoEstilo.addEventListener("click", toggleMenu);
 
 document.addEventListener("click", function (event) {
     const dentroDoMenu = menuEstilo.contains(event.target);
     const clicouNoBotao = botaoEstilo.contains(event.target);
 
     if (!dentroDoMenu && !clicouNoBotao && menuAberto) {
-        menuEstilo.classList.remove("ativo");
-        menuEstilo.classList.add("fechar");
-        menuAberto = false;
+        fecharMenu();
     }
 });
