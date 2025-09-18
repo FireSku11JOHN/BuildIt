efceb2f0ae8e2d9441cc64f2ad6576be8dc735c5
John Ávylan <avylanjohn@gmail.com>
2025-09-18 10:40:52 -0300

    Removido arquivos Node.js e corrigido problemas relacionados ao cabeçalho

--- script/btnLimparBusca.js
+++ script/btnLimparBusca.js
@@ -1,13 +1,15 @@
-const btnLimparBusca = document.querySelector('[data-btn-limpar]');
-const inputBuscar = document.querySelector('[data-input-buscar]');
+document.addEventListener('DOMContentLoaded', () => {
+    const btnLimparBusca = document.querySelector('[data-btn-limpar]');
+    const inputBuscar = document.querySelector('[data-input-buscar]');
 
-btnLimparBusca.addEventListener('click', () => {
-    inputBuscar.value = '';
-});
+    btnLimparBusca.addEventListener('click', () => {
+        inputBuscar.value = '';
+    });
 
-const btnLimparBuscaML = document.querySelector('[data-btn-limpar-ml]');
-const inputBuscarML = document.querySelector('[data-input-buscar-menu-lateral]');
+    const btnLimparBuscaML = document.querySelector('[data-btn-limpar-ml]');
+    const inputBuscarML = document.querySelector('[data-input-buscar-menu-lateral]');
 
-btnLimparBuscaML.addEventListener('click', () => {
-    inputBuscarML.value = '';
+    btnLimparBuscaML.addEventListener('click', () => {
+        inputBuscarML.value = '';
+    });
 });
--- script/buscar.js
+++ script/buscar.js
@@ -1,61 +1,62 @@
 import { dadosCard } from "../dados/dadosCard.js";
 import { criarPaginaCard } from './criarPaginaCard.js';
 
-const input = document.querySelector('[data-input-buscar]');
-const ul = document.querySelector('[data-ul-resultado]');
-const divResultado = document.querySelector('[data-div-resultado]');
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelector('[data-input-buscar]');
+  const ul = document.querySelector('[data-ul-resultado]');
+  const divResultado = document.querySelector('[data-div-resultado]');
 
-input.addEventListener('input', () => {
-    const termo = input.value.toLowerCase().trim();
-    ul.innerHTML = ''; // Limpa resultados antigos
-    divResultado.style.display = 'block'
+  input.addEventListener('input', () => {
+      const termo = input.value.toLowerCase().trim();
+      ul.innerHTML = ''; // Limpa resultados antigos
+      divResultado.style.display = 'block'
 
-    const resultados = dadosCard.filter(item =>
-      item.titulo.toLowerCase().includes(termo) ||
-      item.descricao.toLowerCase().includes(termo) ||
-      item.tag.toLowerCase().includes(termo)
-    );
+      const resultados = dadosCard.filter(item =>
+        item.titulo.toLowerCase().includes(termo) ||
+        item.descricao.toLowerCase().includes(termo) ||
+        item.tag.toLowerCase().includes(termo)
+      );
 
-    if (resultados.length === 0) {
-      const li = document.createElement('li');
-      li.classList.add('li-resultado-busca');
-      li.innerHTML = `
-        <p>Nenhum resultado encontrado para: <span class="msg-erro-busca">${input.value}</span></p>
-      `;
-      ul.appendChild(li);
-      // divResultado.style.padding = '10px';
-      return;
-    }
+      if (resultados.length === 0) {
+        const li = document.createElement('li');
+        li.classList.add('li-resultado-busca');
+        li.innerHTML = `
+          <p>Nenhum resultado encontrado para: <span class="msg-erro-busca">${input.value}</span></p>
+        `;
+        ul.appendChild(li);
+        // divResultado.style.padding = '10px';
+        return;
+      }
 
 
-    resultados.forEach(card => {
-      const li = document.createElement('li');
-      li.classList.add('li-resultado-busca');
-      li.innerHTML = `
-        <p>${card.titulo}</p>
-        <img src="${card.imagem}" alt="${card.titulo}" class="img-resultado-busca">
-      `;
+      resultados.forEach(card => {
+        const li = document.createElement('li');
+        li.classList.add('li-resultado-busca');
+        li.innerHTML = `
+          <p>${card.titulo}</p>
+          <img src="${card.imagem}" alt="${card.titulo}" class="img-resultado-busca">
+        `;
 
-      li.addEventListener('click', () => {
-        criarPaginaCard(card); //  Redireciona para card.html
+        li.addEventListener('click', () => {
+          criarPaginaCard(card); //  Redireciona para card.html
+        });
+
+        ul.appendChild(li);
+
+        divResultado.style.padding = '10px';
       });
-
-      ul.appendChild(li);
-
-      divResultado.style.padding = '10px';
-    });
-});
+  });
 
 
-document.addEventListener('click', (event) => {
-const isClickInsideInput = input.contains(event.target);
-const isClickInsideResults = divResultado.contains(event.target);
+  document.addEventListener('click', (event) => {
+  const isClickInsideInput = input.contains(event.target);
+  const isClickInsideResults = divResultado.contains(event.target);
 
-if (!isClickInsideInput && !isClickInsideResults) {
-  divResultado.style.display = 'none';
-} else {
-  divResultado.style.display = 'block';
-} 
-});
-
+  if (!isClickInsideInput && !isClickInsideResults) {
+    divResultado.style.display = 'none';
+  } else {
+    divResultado.style.display = 'block';
+  } 
+  });
+})
 
