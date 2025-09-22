d48cde924e3e724cd2cef331706d1514e90d1fde
John Avylan <avylanjohn@gmail.com>
2025-09-21 21:10:16 -0300

    inicio de cadastrar.html

--- script/login.js
+++ script/login.js
@@ -1,7 +1,16 @@
 export function login() {
     document.addEventListener('click', (event) => {
+
+        const btnCadastrar = document.getElementById('btnCadastrar');
+
         if (event.target && event.target.id === 'btnEntrar') {
             window.location.href = '../pages/login.html';
+            console.log('Clicou no botão Entrar', event.target);
+        }
+
+        if (event.target && event.target.id === 'btnCadastrar') {
+            console.log('Clicou no botão Cadastrar', event.target);
+            // window.location.href = '../pages/cadastrar.html';
         }
     });
 }
