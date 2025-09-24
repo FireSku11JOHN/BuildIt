231581831e31fa3d3ce9a31c8c566cb43caffb1e
John Avylan <avylanjohn@gmail.com>
2025-09-24 18:19:58 -0300

    Removendo arquivos de teste do versionamento

--- script/login.js
+++
@@ -1,7 +0,0 @@
-export function login() {
-    document.addEventListener('click', (event) => {
-        if (event.target && event.target.id === 'btnEntrar') {
-            window.location.href = '../pages/login.html';
-        }
-    });
-}
